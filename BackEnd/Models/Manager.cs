3bc3f68f8523255a8ece67296e2cc78a4be9ce14
Ngô An Bình - HE170185 <binhnahe170185@fpt.edu.com>
2025-09-24 12:30:36 +0700

    Create Manager model and ManagersCotroller file

---
+++ BackEnd/Models/Manager.cs
@@ -0,0 +1,9 @@
+namespace FAJP.Models
+{
+    public class Manager
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
