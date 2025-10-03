53adcae4ca0e52698a4352ca634b30e7dcc6de00
Ngô An Bình - HE170185 <binhnahe170185@fpt.edu.com>
2025-10-03 14:01:35 +0700

    feat: allow managers to toggle class status

--- BackEnd/Models/Class.cs
+++ BackEnd/Models/Class.cs
@@ -1,12 +1,14 @@
+using System;
+
 namespace FAJP.Models
 {
     public class Class
     {
         public string class_id { get; set; }
         public string class_name { get; set; }
-        public string subject { get; set; }
-        public string teacher { get; set; }
-        public string room_name { get; set; }
-        public int students { get; set; }
+        public string semester { get; set; }
+        public DateTime? start_date { get; set; }
+        public DateTime? end_date { get; set; }
+        public string status { get; set; }
     }
 }
