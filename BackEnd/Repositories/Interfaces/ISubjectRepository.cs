00f348ffeba71e84a12724c0dba05a6e84028ab6
duongodayne <thuyduong17092003@gmail.com>
2025-10-11 23:29:58 +0700

    change subject repo

--- BackEnd/Repositories/Interfaces/ISubjectRepository.cs
+++ BackEnd/Repositories/Interfaces/ISubjectRepository.cs
@@ -1,13 +1,20 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 using System.Threading.Tasks;
 >>>>>>> 179db62 (View list material, create api for subject)
+=======
+using System.Threading.Tasks;
+=======
+>>>>>>> master
+>>>>>>> ba272be (change subject repo)
 using FJAP.Models;
 
 namespace FJAP.Repositories.Interfaces
 {
     public interface ISubjectRepository : IGenericRepository<Subject>
     {
+<<<<<<< HEAD
 <<<<<<< HEAD
         Task<SubjectDto?> GetByIdWithDetailsAsync(int id);
         Task<IEnumerable<SubjectDto>> GetAllWithDetailsAsync();
@@ -17,5 +24,16 @@ namespace FJAP.Repositories.Interfaces
         Task<Subject?> GetDetailAsync(int id);
         // Thêm method đặc thù nếu cần
 >>>>>>> 179db62 (View list material, create api for subject)
+=======
+        Task<Subject?> GetDetailAsync(int id);
+        // Thêm method đặc thù nếu cần
+=======
+        Task<SubjectDto?> GetByIdWithDetailsAsync(int id);
+        Task<IEnumerable<SubjectDto>> GetAllWithDetailsAsync();
+        Task UpdateStatusAsync(int subjectId, string status);
+        Task<SubjectFormOptions> GetFormOptionsAsync();
+
+>>>>>>> master
+>>>>>>> ba272be (change subject repo)
     }
 }
