fd0220504b6b29fca82285facb8c576d71117080
LeQuangHuy-HE171807 <huylqhe171807@fpt.edu.vn>
2025-10-14 00:21:20 +0700

    [HuyLQ} - FE BE - Rebuild project

---
+++ FjapBE/vn.fpt.edu.repositories/Interfaces/IAuthRepository.cs
@@ -0,0 +1,15 @@
+﻿using FJAP.Models;
+using FJAP.Repositories.Interfaces;
+using Google.Apis.Auth;
+
+public interface IAuthRepository : IGenericRepository<Account>
+{
+    Task<Account?> GetByEmailAsync(string email);
+    //ask<bool> ValidatePasswordAsync(string email, string password);
+    Task<GoogleJsonWebSignature.Payload?> VerifyGoogleTokenAsync(string idToken, string clientId);
+
+    Task<int?> GetStudentIdByUserIdAsync(int userId);
+    Task<int?> GetLectureIdByUserIdAsync(int userId);
+
+
+}
