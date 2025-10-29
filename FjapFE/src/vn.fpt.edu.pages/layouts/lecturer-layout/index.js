d070cc6bf938649ab1148421f2dee717c4f38545
Ngô An Bình - HE170185 <binhnahe170185@fpt.edu.vn>
2025-10-29 21:24:53 +0700

    Fix lecturer home page UI

--- FjapFE/src/vn.fpt.edu.pages/layouts/lecturer-layout/index.js
+++ FjapFE/src/vn.fpt.edu.pages/layouts/lecturer-layout/index.js
@@ -1,51 +1,8 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
-import LecturerHeader from "./lecturer-header";
-import LecturerFooter from "./lecturer-footer";
-import LecturerSidebar from "./lecturer-sidebar";
+import React from 'react';
+import LecturerHomepage from './LecturerHomepage';
 
-const layoutStyles = {
-  minHeight: "100vh",
-  display: "flex",
-  flexDirection: "column",
-  backgroundColor: "#f8fafc",
-};
-
-const bodyStyles = {
-  display: "flex",
-  flex: 1,
-  overflow: "hidden",
-};
-
-const sidebarStyles = {
-  width: 240,
-  borderRight: "1px solid #f0f0f0",
-  background: "#fff",
-  flexShrink: 0,
-};
-
-const mainStyles = {
-  flex: 1,
-  padding: "24px 32px",
-  background: "#fff",
-  overflowY: "auto",
-};
-
-const LecturerLayout = ({ children }) => {
-  const bodyContent = children ?? <Outlet />;
-
-  return (
-    <div style={layoutStyles}>
-      <LecturerHeader />
-      <div style={bodyStyles}>
-        <aside style={sidebarStyles}>
-          <LecturerSidebar />
-        </aside>
-        <main style={mainStyles}>{bodyContent}</main>
-      </div>
-      <LecturerFooter />
-    </div>
-  );
+const LecturerLayout = () => {
+  return <LecturerHomepage />;
 };
 
 export default LecturerLayout;
