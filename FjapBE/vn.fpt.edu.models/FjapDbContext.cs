78ee3ace4a7487e2d506d9b75ce0e0fb7b2257b3
Ngô An Bình - HE170185 <binhnahe170185@fpt.edu.vn>
2025-10-30 09:46:42 +0700

    Remove local DbContext config and fix connection string user

--- FjapBE/vn.fpt.edu.models/FjapDbContext.cs
+++ FjapBE/vn.fpt.edu.models/FjapDbContext.cs
@@ -67,10 +67,6 @@ public partial class FjapDbContext : DbContext
 
     public virtual DbSet<User> Users { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseMySql("server=127.0.0.1;port=3306;database=fjap;user=root;password=123456;sslmode=None;allowpublickeyretrieval=True", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.22-mysql"));
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder
