f717e85579231ebb8b8565826f6ed07fce836c5c
Ngô An Bình - HE170185 <binhnahe170185@fpt.edu.vn>
2025-11-04 21:24:53 +0700

    CORS: allow Azure Static Web Apps origin for login

--- FjapBE/vn.fpt.edu.infrastructure/Extensions/CorsExtensions.cs
+++ FjapBE/vn.fpt.edu.infrastructure/Extensions/CorsExtensions.cs
@@ -10,7 +10,7 @@ namespace FJAP.Infrastructure.Extensions
         public static IServiceCollection AddAppCors(this IServiceCollection services, IConfiguration config)
         {
             var origins = config.GetSection("Frontend:Origins").Get<string[]>()
-                          ?? new[] { "http://localhost:3000" };
+                          ?? new[] { "http://localhost:3000", "https://gray-plant-0778b1000.3.azurestaticapps.net" };
 
             services.AddCors(opt =>
             {
