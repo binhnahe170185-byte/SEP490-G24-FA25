670c4821681756041b0e0d51b4d1d07a0f42beb2
TrinhVanSang03 <sangtvhe172283@fpt.edu.vn>
2025-12-18 23:02:08 +0700

    [SangTV]_BE_Fix bug grade

--- FjapBE/DTOs/GradeDtos.cs
+++ FjapBE/DTOs/GradeDtos.cs
@@ -65,6 +65,7 @@ namespace FJAP.vn.fpt.edu.models
         public int? SemesterId { get; set; }
         public string? Status { get; set; }
         public string? SearchTerm { get; set; } // Tìm theo tên/mã sinh viên
+        public int? UserId { get; set; } 
         public int PageNumber { get; set; } = 1;
         public int PageSize { get; set; } = 20;
     }
